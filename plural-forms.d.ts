bb2c6e4fcea145d997a2937024400468f3783c39
Gintautas Miselis <gintautas@miselis.lt>
2024-10-02 21:54:12 +0300

    pluralFunc can return undefined if value array is shorter than max n

--- plural-forms.d.ts
+++ plural-forms.d.ts
@@ -1,6 +1,6 @@
 export declare function getFormula(language: string): string;
 export declare function getNPlurals(language: string): number;
-export declare function getPluralFunc(language: string): (plural: number, wordForms: string[]) => string;
+export declare function getPluralFunc(language: string): (plural: number, wordForms: string[]) => string | undefined;
 export declare function hasLang(language: string): boolean;
 export declare function getPluralFormsHeader(language: string): string;
 export declare function getAvailLangs(): string[];
